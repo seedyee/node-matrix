8dd60599b2208f0b8f7e73b72a4fdcc49a56a095
vimniky <vimniky@gmail.com>
2016-12-16 10:35:38 +0800

    Clean up

--- red/runtime/nodes/Node.js
+++ red/runtime/nodes/Node.js
@@ -1,48 +1,48 @@
-var util = require("util");
-var EventEmitter = require("events").EventEmitter;
-var when = require("when");
+var util = require('util')
+var EventEmitter = require('events').EventEmitter
+var when = require('when')
 
-var redUtil = require("../util");
-var Log = require("../log");
-var context = require("./context");
-var flows = require("./flows");
+var redUtil = require('../util')
+var Log = require('../log')
+var context = require('./context')
+var flows = require('./flows')
 
 function Node(n) {
-  this.id = n.id;
-  this.type = n.type;
-  this.z = n.z;
-  this._closeCallbacks = [];
+  this.id = n.id
+  this.type = n.type
+  this.z = n.z
+  this._closeCallbacks = []
 
   if (n.name) {
-    this.name = n.name;
+    this.name = n.name
   }
   if (n._alias) {
-    this._alias = n._alias;
+    this._alias = n._alias
   }
-  this.updateWires(n.wires);
+  this.updateWires(n.wires)
 }
 
-util.inherits(Node, EventEmitter);
+util.inherits(Node, EventEmitter)
 
 Node.prototype.updateWires = function(wires) {
-  //console.log("UPDATE",this.id);
-  this.wires = wires || [];
-  delete this._wire;
+  //console.log('UPDATE',this.id)
+  this.wires = wires || []
+  delete this._wire
 
-  var wc = 0;
+  var wc = 0
   this.wires.forEach(function(w) {
-    wc += w.length;
-  });
-  this._wireCount = wc;
+    wc += w.length
+  })
+  this._wireCount = wc
   if (wc === 0) {
     // With nothing wired to the node, no-op send
     this.send = function(msg) {}
   } else {
-    this.send = Node.prototype.send;
+    this.send = Node.prototype.send
     if (this.wires.length === 1 && this.wires[0].length === 1) {
       // Single wire, so we can shortcut the send when
       // a single message is sent
-      this._wire = this.wires[0][0];
+      this._wire = this.wires[0][0]
     }
   }
 
@@ -50,114 +50,114 @@ Node.prototype.updateWires = function(wires) {
 
 Node.prototype.context = function() {
   if (!this._context) {
-    this._context = context.get( this._alias || this.id, this.z);
+    this._context = context.get( this._alias || this.id, this.z)
   }
-  return this._context;
+  return this._context
 }
 
-Node.prototype._on = Node.prototype.on;
+Node.prototype._on = Node.prototype.on
 
 Node.prototype.on = function(event, callback) {
-  var node = this;
-  if (event == "close") {
-    this._closeCallbacks.push(callback);
+  var node = this
+  if (event == 'close') {
+    this._closeCallbacks.push(callback)
   } else {
-    this._on(event, callback);
+    this._on(event, callback)
   }
-};
+}
 
 Node.prototype.close = function() {
-  var promises = [];
-  var node = this;
+  var promises = []
+  var node = this
   this._closeCallbacks.forEach(callback => {
     if (callback.length == 1) {
       promises.push(
         when.promise(function(resolve) {
           callback.call(node, function() {
-            resolve();
-          });
+            resolve()
+          })
         })
-      );
+      )
     } else {
-      callback.call(node);
+      callback.call(node)
     }
   })
   if (promises.length > 0) {
     return when.settle(promises).then(function() {
       if (this._context) {
-        context.delete(this._alias||this.id,this.z);
+        context.delete(this._alias||this.id,this.z)
       }
-    });
+    })
   } else {
     if (this._context) {
-      context.delete(this._alias||this.id,this.z);
+      context.delete(this._alias||this.id,this.z)
     }
-    return;
+    return
   }
-};
+}
 
 Node.prototype.send = function(msg) {
-  var msgSent = false;
-  var node;
-  if (msg === null || typeof msg === "undefined") {
-    return;
+  var msgSent = false
+  var node
+  if (msg === null || typeof msg === 'undefined') {
+    return
   } else if (!util.isArray(msg)) {
     if (this._wire) {
       // A single message and a single wire on output 0
       // TODO: pre-load flows.get calls - cannot do in constructor
       //       as not all nodes are defined at that point
       if (!msg._msgid) {
-        msg._msgid = redUtil.generateId();
+        msg._msgid = redUtil.generateId()
       }
-      this.metric("send", msg);
-      node = flows.get(this._wire);
+      this.metric('send', msg)
+      node = flows.get(this._wire)
       /* istanbul ignore else */
       if (node) {
-        node.receive(msg);
+        node.receive(msg)
       }
-      return;
+      return
     } else {
-      msg = [msg];
+      msg = [msg]
     }
   }
 
-  var numOutputs = this.wires.length;
+  var numOutputs = this.wires.length
 
   // Build a list of send events so that all cloning is done before
   // any calls to node.receive
-  var sendEvents = [];
+  var sendEvents = []
 
-  var sentMessageId = null;
+  var sentMessageId = null
 
   // for each output of node eg. [msgs to output 0, msgs to output 1, ...]
   for (var i = 0; i < numOutputs; i++) {
-    var wires = this.wires[i]; // wires leaving output i
+    var wires = this.wires[i] // wires leaving output i
     /* istanbul ignore else */
     if (i < msg.length) {
-      var msgs = msg[i]; // msgs going to output i
-      if (msgs !== null && typeof msgs !== "undefined") {
+      var msgs = msg[i] // msgs going to output i
+      if (msgs !== null && typeof msgs !== 'undefined') {
         if (!util.isArray(msgs)) {
-          msgs = [msgs];
+          msgs = [msgs]
         }
-        var k = 0;
+        var k = 0
         // for each recipent node of that output
         for (var j = 0; j < wires.length; j++) {
-          node = flows.get(wires[j]); // node at end of wire j
+          node = flows.get(wires[j]) // node at end of wire j
           if (node) {
             // for each msg to send eg. [[m1, m2, ...], ...]
             for (k = 0; k < msgs.length; k++) {
-              var m = msgs[k];
+              var m = msgs[k]
               if (m !== null && m !== undefined) {
                 /* istanbul ignore else */
                 if (!sentMessageId) {
-                  sentMessageId = m._msgid;
+                  sentMessageId = m._msgid
                 }
                 if (msgSent) {
-                  var clonedmsg = redUtil.cloneMessage(m);
-                  sendEvents.push({n:node,m:clonedmsg});
+                  var clonedmsg = redUtil.cloneMessage(m)
+                  sendEvents.push({n:node,m:clonedmsg})
                 } else {
-                  sendEvents.push({n:node,m:m});
-                  msgSent = true;
+                  sendEvents.push({n:node,m:m})
+                  msgSent = true
                 }
               }
             }
@@ -168,34 +168,34 @@ Node.prototype.send = function(msg) {
   }
   /* istanbul ignore else */
   if (!sentMessageId) {
-    sentMessageId = redUtil.generateId();
+    sentMessageId = redUtil.generateId()
   }
-  this.metric("send",{_msgid:sentMessageId});
+  this.metric('send',{_msgid:sentMessageId})
 
-  for (i=0;i<sendEvents.length;i++) {
-    var ev = sendEvents[i];
+  for (i=0; i<sendEvents.length; i++) {
+    var ev = sendEvents[i]
     /* istanbul ignore else */
     if (!ev.m._msgid) {
-      ev.m._msgid = sentMessageId;
+      ev.m._msgid = sentMessageId
     }
-    ev.n.receive(ev.m);
+    ev.n.receive(ev.m)
   }
-};
+}
 
 Node.prototype.receive = function(msg) {
   if (!msg) {
-    msg = {};
+    msg = {}
   }
   if (!msg._msgid) {
-    msg._msgid = redUtil.generateId();
+    msg._msgid = redUtil.generateId()
   }
-  this.metric("receive",msg);
+  this.metric('receive',msg)
   try {
-    this.emit("input", msg);
+    this.emit('input', msg)
   } catch(err) {
-    this.error(err,msg);
+    this.error(err,msg)
   }
-};
+}
 
 function log_helper(self, level, msg) {
   var o = {
@@ -203,52 +203,52 @@ function log_helper(self, level, msg) {
     id: self.id,
     type: self.type,
     msg: msg
-  };
-  if (self.name) {
-    o.name = self.name;
   }
-  Log.log(o);
+  if (self.name) {
+    o.name = self.name
+  }
+  Log.log(o)
 }
 
 Node.prototype.log = function(msg) {
-  log_helper(this, Log.INFO, msg);
-};
+  log_helper(this, Log.INFO, msg)
+}
 
 Node.prototype.warn = function(msg) {
-  log_helper(this, Log.WARN, msg);
-};
+  log_helper(this, Log.WARN, msg)
+}
 
 Node.prototype.error = function(logMessage,msg) {
   if (typeof logMessage != 'boolean') {
-    logMessage = logMessage || "";
+    logMessage = logMessage || ''
   }
-  log_helper(this, Log.ERROR, logMessage);
+  log_helper(this, Log.ERROR, logMessage)
   /* istanbul ignore else */
   if (msg) {
-    flows.handleError(this,logMessage,msg);
+    flows.handleError(this,logMessage,msg)
   }
-};
+}
 
 /**
  * If called with no args, returns whether metric collection is enabled
  */
 Node.prototype.metric = function(eventname, msg, metricValue) {
-  if (typeof eventname === "undefined") {
-    return Log.metric();
+  if (typeof eventname === 'undefined') {
+    return Log.metric()
   }
-  var metrics = {};
-  metrics.level = Log.METRIC;
-  metrics.nodeid = this.id;
-  metrics.event = "node."+this.type+"."+eventname;
-  metrics.msgid = msg._msgid;
-  metrics.value = metricValue;
-  Log.log(metrics);
+  var metrics = {}
+  metrics.level = Log.METRIC
+  metrics.nodeid = this.id
+  metrics.event = 'node.'+this.type+'.'+eventname
+  metrics.msgid = msg._msgid
+  metrics.value = metricValue
+  Log.log(metrics)
 }
 
 /**
- * status: { fill:"red|green", shape:"dot|ring", text:"blah" }
+ * status: { fill:'red|green', shape:'dot|ring', text:'blah' }
  */
 Node.prototype.status = function(status) {
-  flows.handleStatus(this,status);
-};
-module.exports = Node;
+  flows.handleStatus(this,status)
+}
+module.exports = Node
