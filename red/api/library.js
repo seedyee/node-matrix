db88cfb0652d62783bc3350def4457285bb242cc
vimniky <vimniky@gmail.com>
2016-12-17 11:56:24 +0800

    Clean up

--- red/api/library.js
+++ red/api/library.js
@@ -1,26 +1,8 @@
-/**
- * Copyright 2013, 2015 IBM Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the 'License');
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
-var fs = require('fs')
-var fspath = require('path')
-var when = require('when')
+const needsPermission = require('./auth').needsPermission
 
-var redApp = null
-var storage
-var log
-var needsPermission = require('./auth').needsPermission
+let redApp = null
+let storage
+let log
 
 function createLibrary(type) {
   if (redApp) {
