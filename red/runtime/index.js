265f59b5796aea94e6f60e89ac542ce21c2fb049
vimniky <vimniky@gmail.com>
2016-12-19 09:03:39 +0800

    Clean up unused variables

--- red/runtime/index.js
+++ red/runtime/index.js
@@ -1,42 +1,38 @@
-const when = require('when')
-const redNodes = require('./nodes')
+const nodes = require('./nodes')
 const storage = require('./storage')
 const log = require('./log')
 const events = require('./events')
-const path = require('path')
-const fs = require('fs')
-const userSettings = require('../../settings')
-const adminApi = require('../api')
+const settings = require('../../settings')
+const api = require('../api')
+const util = require('./util')
 
-const { version } = userSettings
 
 function init() {
-  redNodes.init(runtime)
+  nodes.init(runtime)
   log.init()
 }
 
 function start() {
   console.log('\n==================== Welcome ============================\n')
-  log.info(`Node-RED version v${version}`)
+  log.info(`Node-RED version v${settings.version}`)
   log.info(`Node.js version ${process.version}`)
-  redNodes.load()
-  return redNodes.loadFlows().then(redNodes.startFlows)
+  nodes.load()
+  return nodes.loadFlows().then(nodes.startFlows)
 }
 
 function stop() {
-  return redNodes.stopFlows()
+  return nodes.stopFlows()
 }
 
 var runtime = module.exports = {
   init,
   start,
   stop,
-  version,
   log,
-  settings: userSettings,
+  settings,
   storage,
   events,
-  nodes: redNodes,
-  util: require('./util'),
-  get adminApi() { return adminApi },
+  nodes: nodes,
+  util,
+  get adminApi() { return api },
 }
