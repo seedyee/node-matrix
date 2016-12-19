a8f03c39db1cb4fa05375abe5f763ba112d5013d
vimniky <vimniky@gmail.com>
2016-12-19 20:47:07 +0800

    Clean up flows api

--- red/api/flows.js
+++ red/api/flows.js
@@ -1,64 +1,23 @@
-/**
- * Copyright 2014, 2016 IBM Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
-
 let log
 let redNodes
-let settings
 
 module.exports = {
   init: function(runtime) {
-    settings = runtime.settings
     redNodes = runtime.nodes
     log = runtime.log
   },
 
   get: function(req, res) {
-    log.audit({event: 'flows.get', version: 'v2' }, req)
     res.json(redNodes.getFlows())
   },
-
   post: function(req, res) {
-    var flows = req.body
-    var deploymentType = req.get('Node-RED-Deployment-Type') || 'full'
-    log.audit({event: 'flows.set', type: deploymentType, version: 'v2' }, req)
-    if (deploymentType === 'reload') {
-      redNodes.loadFlows().then(function() {
-        res.status(204).end()
-      }).otherwise(function(err) {
-        log.warn(`api.flows.error-reload ${err.message}`)
-        log.warn(err.stack)
-        res.status(500).json({error: 'unexpected_error', message:err.message})
-      })
-    } else {
-      var flowConfig = flows
-      flowConfig = flows.flows
-      if (flows.hasOwnProperty('rev')) {
-        var currentVersion = redNodes.getFlows().rev
-        if (currentVersion !== flows.rev) {
-          //TODO: log warning
-          return res.status(409).json({code:'version_mismatch'})
-        }
-      }
-      redNodes.setFlows(flowConfig,deploymentType).then(function(flowId) {
-        res.json({rev:flowId})
-      }).otherwise(function(err) {
-        log.warn(`api.flows.error-save ${err.message}`)
-        log.warn(err.stack)
-        res.status(500).json({error: 'unexpected_error', message: err.message})
-      })
-    }
+    const flows = req.body
+    redNodes.setFlows(flows.flows).then(function(flowId) {
+      res.json({ rev: flowId })
+    }).catch(function(err) {
+      log.warn(`api.flows.error-save ${err.message}`)
+      log.warn(err.stack)
+      res.status(500).json({error: 'unexpected_error', message: err.message})
+    })
   }
 }
